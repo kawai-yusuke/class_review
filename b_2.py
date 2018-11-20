938266235377557723b73be586c2a75ed7e1adeb
Yusuke <homekawai22goal@yahoo.co.jp>
2018-11-20 22:44:27 +0900

    関数を呼び出すとき、()をつけ忘れた
    self.ageという名前がかぶってしまった

---
+++ b_2.py
@@ -0,0 +1,21 @@
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.toshi = age
+
+    def full_name(self):
+        print(f"{self.first_name} {self.family_name}")
+
+    def age(self):
+        print(self.toshi)
+
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.age()  # 15 という値を返す
+
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+tom.age()  # 57 という値を返す
+
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.age()  # 73 という値を返す
