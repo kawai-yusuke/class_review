f31cf441ca82eeacadbc10737abc1b8fcb962e97
Yusuke <homekawai22goal@yahoo.co.jp>
2018-11-20 23:47:08 +0900

    これでいいのだろうか

---
+++ b_6.py
@@ -0,0 +1,26 @@
+class Customer:
+    def __init__(self, first_name, family_name, age):
+        self.first_name = first_name
+        self.family_name = family_name
+        self.toshi = age
+
+    def full_name(self):
+        return f"{self.first_name} {self.family_name}"
+
+    def age(self):
+        return self.toshi
+
+    def entry_fee(self):
+        if self.toshi <= 3:
+            return 0
+        if self.toshi < 20:
+            return 1000
+        if 20 <= self.toshi < 65:
+            return 1500
+        if self.toshi >= 75:
+            return 500
+        if self.toshi >= 65:
+            return 1200
+
+    def info_csv(self):
+        print(f"{self.full_name()}, {self.age()}, {self.entry_fee()}")
