2ff80eae7ef07f998bbdd45e3a810108ef1379e6
Yusuke <homekawai22goal@yahoo.co.jp>
2018-11-24 23:09:48 +0900

    宿題の修正

--- b_8.py
+++ b_8.py
@@ -2,28 +2,28 @@ class Customer:
     def __init__(self, first_name, family_name, age):
         self.first_name = first_name
         self.family_name = family_name
-        self.toshi = age
+        self.age = age
 
     def full_name(self):
         return f"{self.first_name} {self.family_name}"
 
-    def age(self):
-        return self.toshi
+    # def age(self):
+    #     return self.toshi
 
     def entry_fee(self):
-        if self.toshi <= 3:
+        if self.age <= 3:
             return 0
-        if self.toshi < 20:
+        if self.age < 20:
             return 1000
-        if 20 <= self.toshi < 65:
+        if 20 <= self.age < 65:
             return 1500
-        if self.toshi >= 75:
+        if self.age >= 75:
             return 500
-        if self.toshi >= 65:
+        if self.age >= 65:
             return 1200
 
     def info_csv(self, between):
-        print(f"{self.full_name()}{between}{self.age()}{between}{self.entry_fee()}")
+        print(f"{self.full_name()}{between}{self.age}{between}{self.entry_fee()}")
 
 
 ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
